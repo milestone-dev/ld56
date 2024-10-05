6b99bddbf5cf1de8ea5a20bfc5db85e6c896296c
Patrik Sjöberg <voxxar@gmail.com>
2024-10-05 20:57:27 +0200

    fixbsp switches to godot for you

--- fixbsp.sh
+++ fixbsp.sh
@@ -5,6 +5,8 @@ echo "You need to close GoDot before running this script. I will kill it with fo
 read -p "Press enter when you have tried to save your work and ask it nicely to close."
 
 killall -9 Godot 2> /dev/null || true
+sleep 2
+osascript -e 'tell application "Terminal" to activate'
 
 # Find all .bsp files and store them in an array
 bsp_files=($(find . -name "*.bsp"))
@@ -12,14 +14,19 @@ bsp_files=($(find . -name "*.bsp"))
 echo "Deleting all bsp files"
 rm assets/maps/*.bsp* 2> /dev/null || true
 
-read -p "Now start GoDot and open the project, then return here and press enter to continue."
+open project.godot
+sleep 3
+osascript -e 'tell application "Terminal" to activate'
+#read -p "Now start GoDot and open the project, then return here and press enter to continue."
 
 # Loop through the same files and run git checkout to restore them
 for file in "${bsp_files[@]}"; do
     echo "Restoring $file"
     git checkout -- "$file"
-    
-    read -p "Switch to GoDot and wait for the import to finish, then come back here and press enter."
+    osascript -e 'tell application "Godot" to activate'
+    sleep 2
+    osascript -e 'tell application "Terminal" to activate'
+    # read -p "Switch to GoDot and wait for the import to finish, then come back here and press enter."
 done
 
 echo "Done!"
