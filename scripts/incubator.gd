a160a7f0d6c9a5f158f7d73e71b3b8034975a563
Fred <frederik.sjolund@gmail.com>
2024-10-06 00:21:31 +0200

    Incubator interactable fix

--- scripts/incubator.gd
+++ scripts/incubator.gd
@@ -2,8 +2,8 @@ extends StaticBody3D
 class_name Incubator
 
 var turned_on : bool = false
-
-@export var max_emissive = 4.0
+var help : bool = false
+@export var max_emissive = 8.0
 
 @onready var incubator: MeshInstance3D = $incubator
 @onready var light: SpotLight3D = $"SpotLight3D"
@@ -15,10 +15,13 @@ func _ready() -> void:
 	material.set_shader_parameter("EmissiveStrength", max_emissive * float(turned_on))
 	print("There is an incubator in the scene")
 	
-	
 func interact(player : Player):
 	print("Incubator is interacted upon")
 	turned_on = !turned_on
 	if turned_on:
-		material.set_shader_parameter("EmissiveStrength", max_emissive * turned_on)
+		material.set_shader_parameter("EmissiveStrength", max_emissive * float(turned_on))
 	light.visible = turned_on
+	
+	
+func _process(delta: float) -> void:
+	help = true
