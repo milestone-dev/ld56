2f99fdbc46535fc4adc0c68f94b9dd4b7077b9f0
Patrik Sjöberg <voxxar@gmail.com>
2024-10-06 19:49:03 +0200

    kittens in hand: use amount ratio; so there is a configurable max amount or particles

--- scripts/kittens_in_hand.gd
+++ scripts/kittens_in_hand.gd
@@ -2,22 +2,13 @@ extends Node3D
 class_name KittensInHand
 
 @export var particles: GPUParticles3D
-@export_range(0, 10000) var kitten_count: int = 0: 
-	get:
-		return particles.amount
-	set(value):
-		if value < 1:
-			particles.emitting = false
-		else:
-			particles.amount = kitten_count
-			particles.emitting = true
+@export_range(0, 10000) var kitten_count: int = 0
 	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	particles.amount_ratio = min(kitten_count / float(particles.amount), 1)
+	particles.emitting = kitten_count > 0
