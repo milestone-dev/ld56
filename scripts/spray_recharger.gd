a115cbc634cdb4dda074cfd86a1b84503eaa1e46
Fred <frederik.sjolund@gmail.com>
2024-10-06 23:00:59 +0200

    Bottlespin

--- scripts/spray_recharger.gd
+++ scripts/spray_recharger.gd
@@ -1,5 +1,9 @@
 extends StaticBody3D
 class_name SprayRecharger
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 func interact(player : Player):
 	player.spray_energy = player.SPRAY_ENERGY_MAX
+	animation_player.play("test")
