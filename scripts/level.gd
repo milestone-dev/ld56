844a7bf8353c646ebec34b663918928c93dd1c49
Milestone <56237952+milestone-dev@users.noreply.github.com>
2024-10-06 12:29:55 +0200

    GFX tweask

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,18 +4,20 @@ extends Node3D
 @export var lightningNodes : Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#0-3
-	env.environment.ssao_enabled = Settings.gfx_quality > 2
-	env.environment.ssr_enabled = Settings.gfx_quality > 2
-	env.environment.fog_enabled = Settings.gfx_quality > 1
-	env.environment.glow_enabled = Settings.gfx_quality > 2
-	env.environment.ssil_enabled = Settings.gfx_quality > 2
-	if Settings.gfx_quality > 0:
+	#GFX Quality is 1-4
+	env.environment.ssao_enabled = Settings.gfx_quality > 3
+	env.environment.ssr_enabled = Settings.gfx_quality > 3
+	env.environment.fog_enabled = Settings.gfx_quality > 2
+	env.environment.glow_enabled = Settings.gfx_quality > 3
+	env.environment.ssil_enabled = Settings.gfx_quality > 3
+	if Settings.gfx_quality > 2:
 		env.environment.reflected_light_source = Environment.REFLECTION_SOURCE_SKY
 	else:
 		env.environment.reflected_light_source = Environment.REFLECTION_SOURCE_DISABLED
 
-	lightningNodes.get_node("%Probes").visible = Settings.gfx_quality > 2
-	lightningNodes.get_node("%Spotlights").visible = Settings.gfx_quality > 1
-	lightningNodes.get_node("%CeilingLights").visible = Settings.gfx_quality > 1
-	lightningNodes.get_node("%GlobalLight").visible = Settings.gfx_quality < 2
+	lightningNodes.get_node("%Probes").visible = Settings.gfx_quality > 3
+	lightningNodes.get_node("%Spotlights").visible = Settings.gfx_quality > 2
+	lightningNodes.get_node("%CeilingLights").visible = Settings.gfx_quality > 2
+
+	# Turn on global light if no ceiling lights
+	lightningNodes.get_node("%GlobalLight").visible = !lightningNodes.get_node("%CeilingLights").visible
