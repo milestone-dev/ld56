6b8fb3b3571279b251634b7af95c83c610c4f946
Milestone <56237952+milestone-dev@users.noreply.github.com>
2024-10-06 12:00:06 +0200

    Use Settings.gfx_quality to adjust WorldEnvironment

---
+++ scripts/level.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var env : WorldEnvironment
+@export var lightningNodes : Node3D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#0-3
+	env.environment.ssao_enabled = Settings.gfx_quality > 2
+	env.environment.ssr_enabled = Settings.gfx_quality > 2
+	env.environment.fog_enabled = Settings.gfx_quality > 1
+	env.environment.glow_enabled = Settings.gfx_quality > 2
+	env.environment.ssil_enabled = Settings.gfx_quality > 2
+	if Settings.gfx_quality > 0:
+		env.environment.reflected_light_source = Environment.REFLECTION_SOURCE_SKY
+	else:
+		env.environment.reflected_light_source = Environment.REFLECTION_SOURCE_DISABLED
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,3 +1,3 @@
 extends Node
 var mouse_sensitivity : float = 0.1
-var gfx_quality : int = 0 #0-3
+var gfx_quality : int = 3 #0-3
