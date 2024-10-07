ab7a422a3c585a37ce0e1c40e822d6ef8bcd6603
Fred <frederik.sjolund@gmail.com>
2024-10-07 18:33:51 +0200

    KittenInHand lag fix

--- scripts/particle_preloads.gd
+++ scripts/particle_preloads.gd
@@ -1,5 +1,7 @@
 extends Node3D
 @onready var spray_particles: GPUParticles3D = $SprayParticles
+@onready var kitten_in_hand: GPUParticles3D = $KittenInHand
 
 func _ready() -> void:
 	spray_particles.emitting = true
+	kitten_in_hand.emitting = true
